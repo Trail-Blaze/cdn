8c694e4403f8cd8c30ccee830475a24b0e217057
Immanuel Daviel Garcia <34188635+AlexDev404@users.noreply.github.com>
2021-11-18 16:49:18 -0600

    Delete push.bat

--- push.bat
+++
@@ -1,6 +0,0 @@
-git config user.name "AlexDev404"
-git config user.email "rexydogwoof01@gmail.com"
-git config user.password "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"
-git add .
-git commit -m "Initial Commit"
-git push origin main
