b3365f29ff0b727d657ebcd07adb05d32b070f1e
Immanuel Daviel Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-01-07 14:24:02 -0600

    Update buildCheck.sh

--- buildCheck.sh
+++ buildCheck.sh
@@ -1,5 +1,6 @@
 i=0
 shopt -s extglob
+THISSCRIPT=build.sh
 FILE=build0.tar.gz
 sudo apt-get install tar
 
@@ -7,18 +8,21 @@ function checkfile {
 if [ -f "$FILE" ]; then
 	echo "$FILE exists."
 	i=$((i+1))
+	FILE=build${i}.tar.gz
 	checkfile
 
 else
-	echo "$FILE does not exist."
-
+	echo "[CREATE] $FILE, since it does not exist already."
 	filename="build${i}.tar.gz"
-	tar -czvf ${filename} build
-	rm -rfv !(${filename})
+	j=$((i-1))
+    	tar -czvf ${filename} build
+	rm -rfv build
+	rm -rfv !(build*)
 
 fi
 }
 checkfile
+
 git add .
 git commit -am "Build Files"
 git fetch --all
