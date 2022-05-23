60bdae643aea7a7eb7b62d92ad4a239b78c81c56
Immanuel Daviel Alexander Garcia <34188635+AlexDev404@users.noreply.github.com>
2022-05-23 10:00:00 -0600

    Create buildCheck_new.sh

---
+++ buildCheck_new.sh
@@ -0,0 +1,30 @@
+i=0
+shopt -s extglob
+THISSCRIPT=buildCheck.sh
+FILE=build_win0.tar.gz
+sudo apt-get install tar
+
+function checkfile {
+if [ -f "$FILE" ]; then
+	echo "$FILE exists."
+	i=$((i+1))
+	FILE=build_win${i}.tar.gz
+	checkfile
+
+else
+	clearName="build_win-latest.tar.gz"
+
+if [ -f "$clearName" ]; then
+	echo "[CREATE] Renaming old 'LATEST' build... to $FILE"
+	mv ${clearName} ${FILE}
+fi
+	echo "[CREATE] Creating NEW 'LATEST' build..."
+    tar -czvf ${clearName} build
+	rm -rfv ${THISSCRIPT}
+	rm -rfv build
+	rm -rfv !(build*)
+	echo "# Build Branch" > README.md
+
+fi
+}
+checkfile
